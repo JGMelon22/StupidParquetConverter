d59167de627a5d15e937e2801ffcb39a5d0e8b8e
JGMelon22 <jgvasconcellos22@gmail.com>
2022-07-01 09:12:45 -0300

    Improving code logic

--- ParquetGUI.py
+++ ParquetGUI.py
@@ -6,10 +6,10 @@ import pandas as pd
 # Window content
 layout = [[sg.Text("Hit OK to generate a CSV of the parquet files")],
           [sg.Text(size=(40, 1), key='-OUTPUT-')],
-          [sg.Button('Ok'), sg.Button('Sair')]]
+          [sg.Button('Ok'), sg.Button('Exit')]]
 
 # Generate the window (form)
-window = sg.Window('Tela Principal', layout)
+window = sg.Window('Main Window', layout)
 
 # Display and interact with the window using an event loop
 
@@ -19,19 +19,21 @@ while True:
     # Dynamic directory
     current_directory = os.getcwd()
 
-    data_dir = Path(current_directory + '/data/')
-    full_df = pd.concat(
-        pd.read_parquet(parquet_file)
-        for parquet_file in data_dir.glob('*.parquet')
-    )
-    full_df.to_csv('Converted.csv')
+    if event == 'Ok':
+        data_dir = Path(current_directory + '/data/')
+        full_df = pd.concat(
+            pd.read_parquet(parquet_file)
+            for parquet_file in data_dir.glob('*.parquet')
+        )
 
-    # Generate a .csv file with the .parquet files merged
-    # Success message when generating the .csv
-    window['-OUTPUT-'].update("CSV generated successfully!")
+        # Generate a .csv file with the .parquet files merged
+        # Success message when generating the .csv
+        full_df.to_csv('Converted.csv')
+        window['-OUTPUT-'].update("CSV generated successfully!")
 
     # See if user wants to quit or window was closed
     if event == sg.WINDOW_CLOSED or event == 'Exit':
+        full_df = ""
         break
 
 # Close window
